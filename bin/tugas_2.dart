05fe770f686acf6c55f4207a2f17f758948d5e77
sergiusgeoffrey <73834938+sergiusgeoffrey@users.noreply.github.com>
2022-03-03 18:43:40 +0700

    Genap Versi 1

--- bin/tugas_2.dart
+++ bin/tugas_2.dart
@@ -1,17 +1,31 @@
-void main(List<String> arguments) {
-  int n = int.parse(arguments[0])
-  for(int i = 1, k = 0; i <= rows; ++i, k = 0)
-    {
-        for(space = 1; space <= rows-i; ++space)
-        {
-            cout <<"  ";
-        }
+import 'dart:io';
 
-        while(k != 2*i-1)
-        {
-            cout << "* ";
-            ++k;
-        }
-        cout << endl;
-    }    
+void genapPattern(int N) {
+  for (int i = 1; i <= N; i++) {
+    for(int j = 1; j <= N+1-i; j++){
+        stdout.write(j);
+    }
+    for(int j = 1; j < i*2-1; j++){
+        stdout.write(" ");
+    }
+    for(int j =N+1-i; j >= 1; j--){
+        stdout.write(j);
+    }
+    print("");
+  }
+}
+void main(List<String> arguments) {
+  stdout.write("Input Number: ");
+  String? dtInput = stdin.readLineSync();
+  int? angka = int.parse(dtInput!);
+
+  if (angka % 2 == 0) {
+    genapPattern(angka);
+  } 
+  else if(angka % 2 == 1) {
+    
+  } 
+  else {
+    print("Invalid Input");
+  }
 }
