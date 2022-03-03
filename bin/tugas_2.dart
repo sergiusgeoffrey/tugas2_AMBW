060fe28123bdf6bd123b00114443c53bf2669074
sergiusgeoffrey <73834938+sergiusgeoffrey@users.noreply.github.com>
2022-03-03 19:08:42 +0700

    Genap Versi 2

--- bin/tugas_2.dart
+++ bin/tugas_2.dart
@@ -1,15 +1,42 @@
 import 'dart:io';
 
-void genapPattern(int N) {
+void genapPattern(int N){ 
+  int kiri=1;
+  int kanan=N;
   for (int i = 1; i <= N; i++) {
     for(int j = 1; j <= N+1-i; j++){
-        stdout.write(j);
+        if(kiri > N){
+          kiri=1;
+        }
+
+        if(kiri % 9 == 0)
+        {
+          stdout.write(9);
+          kiri++;
+        }
+        else{ 
+          stdout.write(kiri % 9);
+          kiri++;
+        }
     }
     for(int j = 1; j < i*2-1; j++){
         stdout.write(" ");
     }
-    for(int j =N+1-i; j >= 1; j--){
-        stdout.write(j);
+    for(int j = N+1-i; j >= 1; j--){
+        if(kanan < 1){
+          kanan=N;
+        }
+
+        if(kanan % 9 == 0)
+        {
+          stdout.write(9);
+          kanan--;
+        }
+        else{
+          stdout.write(kanan % 9);
+          kanan--;
+        }
+        
     }
     print("");
   }
